7520580f3f25059ed405a025941d9affa574ab97
aquaraga <aquaraga@gmail.com>
2019-04-06 12:39:50 +0530

    Solutions for demos - 1

--- src/main/kotlin/com/thoughtworks/kotlin/reactive/demo/01_publishers.kt
+++ src/main/kotlin/com/thoughtworks/kotlin/reactive/demo/01_publishers.kt
@@ -1,10 +1,26 @@
 package com.thoughtworks.kotlin.reactive.demo
 
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import java.util.function.Predicate
 
 //Produce a stream of data
 
 
+
+
+
+
 //Define a stream with 3 integers
+fun main(args: Array<String>) {
+    Flux.just(1, 2, 3)
+
+    val numbers = listOf(1, 2, 3, 4)
+    Flux.fromIterable(numbers)
+
+
+    Mono.just("Alice")
+}
+
 
 //Define a stream with one string
--- src/main/kotlin/com/thoughtworks/kotlin/reactive/demo/02_subscribers.kt
+++ src/main/kotlin/com/thoughtworks/kotlin/reactive/demo/02_subscribers.kt
@@ -7,6 +7,35 @@ import reactor.core.publisher.Flux
 //Subscribe to a flux, and add those elements into a list. Print the list
 //1. Use subscriber interface
 //2. Use short version
+fun main(args: Array<String>) {
+
+    val elements = mutableListOf<Int>()
+
+
+    val publisher = Flux.just(1, 2, 3)
+
+    publisher.subscribe(object: Subscriber<Int>{
+        override fun onComplete() {
+        }
+
+        override fun onSubscribe(s: Subscription?) {
+        }
+
+        override fun onNext(t: Int) {
+            elements.add(t)
+        }
+
+        override fun onError(t: Throwable?) {
+        }
+    })
+
+    println(elements)
+
+
+}
+
+
+
 
 
 //Log the operations
